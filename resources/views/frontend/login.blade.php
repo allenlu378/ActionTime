a33bb17cc30996b61c82d99f5f066a2f367de180
allen <allenlu378@gmail.com>
2019-06-27 16:16:12 -0400

    Finish Login

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -1,11 +1,11 @@
 @extends('frontend/layout')
 @section('content')
     <div class="content">
-        <div class='left'><span class = 'block-title'>Public Challenges</span>
+        <div class='left'><img class = 'block-icons' src = "{{asset('frontend/images/public_challenges.png')}}"><span class = 'block-title'>Public Challenges</span>
         </div>
-        <div class='middle'><span class = 'block-title'>Login/Sign Up</span>
+        <div class='middle'><img class = 'block-icons' src = "{{asset('frontend/images/login.png')}}"><span class = 'block-title'>Login/Sign Up</span>
         </div>
-        <div class='right'><span class = 'block-title'>Learn More</span>
+        <div class='right'><img class = 'block-icons' src = "{{asset('frontend/images/info.png')}}"><span class = 'block-title'>Learn More</span>
         </div>
     </div>
 
