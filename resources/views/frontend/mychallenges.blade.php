be6a6fdd4a16f279ea82202af1a477888f0c753d
Steven Cassidy <scass0807@gmail.com>
2019-07-10 11:36:12 -0400

    added my challenges

---
+++ resources/views/frontend/mychallenges.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.blank')
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <h1>My Challenges</h1>
+            <div class="row pending-challenges">
+                <h2>
+                    Pending Challenges
+                </h2>
+                <div class="row pending-container">
+
+                </div>
+            </div>
+            <div class="row accepted-challenges">
+                <h2>
+                    Accepted Challenges
+                </h2>
+                <div class="row accepted-container">
+
+                </div>
+            </div>
+            <div class="row created-challenges">
+                <h2>
+                    Created Challenges
+                </h2>
+                <div class="row created-container">
+
+                </div>
+            </div>
+            <div class="row compl-challenges">
+                <h2>
+                    Created Challenges
+                </h2>
+                <div class="row created-container">
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
